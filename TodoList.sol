ac9c10d4e7b694fe7cf76298bdc8c901f64a1d9b
sweeneyc1 <47680261+sweeneyc1@users.noreply.github.com>
2020-11-15 13:50:58 -0500

    Update TodoList.sol

--- TodoList.sol
+++ TodoList.sol
@@ -15,8 +15,40 @@ contract TodoList{
     function createTask(string memory _name, string memory _desc, uint _prio, uint _dueDate) public{
         _dueDate = block.timestamp + _dueDate;
         
+        if(_prio > 5 ){
+            _prio = 5;
+        }
         generalTasks.push(task(_name,_desc,_prio,_dueDate,false));
         
+    }
+    
+    function removeTask(uint _index) public returns(bool){
+        task storage taskToRemove = generalTasks[_index]; 
+        //remove task
+    }
+    
+    function editName(string memory _name) public {
+        this.name = _name;
+        
+    }
+    
+    function editDescription(string memory _description) public {
+        this.description = _description;
+    }
+    
+    function editPriority(uint _prio) public {
+        if(_prio > 5 ){
+            _prio = 5;
+        }
+        this.priority = _prio;
+    }
+    
+    function editDueDate(uint _dueDate) public {
+        this.dueDate = _dueDate;
+    }
+    
+    function markDone() public {
+        this.completed = true;
     }
     
     function getTimeLeft(uint _index) public view returns (uint){
