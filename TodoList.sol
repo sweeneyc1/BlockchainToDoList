2ebe80044bc35b1f4d1ef730434a4bbbd3d0455d
lutebarnett <lute.barnett@gmail.com>
2020-11-22 17:09:59 -0500

    comments added to methods

--- TodoList.sol
+++ TodoList.sol
@@ -22,8 +22,9 @@ contract TodoList{
     
     address owner = (msg.sender);
     
+    //Creates a task and adds it to the tasks array
     function createTask(string memory _name, string memory _desc, uint _prio, uint _dueDate) public{
-        require(msg.sender==owner);
+        require(msg.sender==owner, "Need to be contract owner");
         _dueDate = block.timestamp + _dueDate;
         
         if(_prio > 5 ){
@@ -33,12 +34,18 @@ contract TodoList{
         
     }
     
+    //removes a task at the given index
     function removeTask(uint _index) public {
-        require(msg.sender==owner);
+        require(msg.sender==owner, "Need to be contract owner");
         delete generalTasks[_index];
     }
     
+    
+    //edits info of a uncompleted tasks
     function editTask(uint _index, string memory _name, string memory _description, uint _prio, uint _dueDate) public {
+        require(msg.sender==owner, "Need to be contract owner");
+        require(!generalTasks[_index].completed, "Task already completed!");
+        
         if(compareStrings(_name, '')){
             generalTasks[_index].name = _name;
         }
@@ -53,20 +60,25 @@ contract TodoList{
         }
     }
     
+    //returns time left in seconds before the task's due date is reached (0 if it has reached)
     function getTimeLeft(uint _index) public view returns (uint){
-        require(msg.sender==owner);
+        require(msg.sender==owner, "Need to be contract owner");
         return (block.timestamp > generalTasks[_index].dueDate ? 0 : generalTasks[_index].dueDate-block.timestamp);
     }
     
+    //returns completed val of a task in the array
     function getCompleted(uint _index) public view returns(bool){
-        require(msg.sender==owner);
+        require(msg.sender==owner, "Need to be contract owner");
         return generalTasks[_index].completed;
     }
     
+    //toggles the completed val of a task in the array
     function toggleComplete(uint _index) public{
-        require(msg.sender==owner);
+        require(msg.sender==owner, "Need to be contract owner");
         generalTasks[_index].completed=!generalTasks[_index].completed;
     }
+    
+    //compares two strings to see if they are the same
     function compareStrings(string memory a, string memory b) public pure returns (bool) {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
     }
