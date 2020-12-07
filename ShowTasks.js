0ae53416e2be1a4c51d8e4169dc760ae470094ef
sweeneyc1 <47680261+sweeneyc1@users.noreply.github.com>
2020-12-07 11:05:49 -0500

    Create ShowTasks.js
    ShowTasks.js

---
+++ ShowTasks.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './ShowTasks.css'
+export class ShowTasks extends Component {
+    handleChange = (task) => {
+        let _task = task;
+        
+        //console.log("Show Tasks: " + task);
+        //where toggle completed
+        this.props.toggleComplete(_task);
+        //console.log("toggle complete");
+    }
+    render() {
+        
+        let taskList = this.props.tasks.map((task, i) =>
+            <tr key={i}>
+                <td onClick={this.handleChange.bind(this, task.name)}>{task.name}</td>
+                <td>{task.description}</td>
+                <td>{task.time}</td>
+                <td>{task.completed}</td>
+            </tr>)
+            
+        return (
+            <div>
+                <h3> Tasks </h3>
+                <hr />
+                <table >
+                    <tbody>
+                        <tr>
+                            <th>Task</th>
+                            <th>Description</th>
+                            <th>Time</th>
+                            <th>Completed</th>
+                        </tr>
+                        {taskList}
+                    </tbody>
+                    
+                </table>
+            </div>
+        )
+    }
+}
+module.export = ShowTasks;
